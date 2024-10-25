c475ebfa90c1aad7fa2b102e832ec132e931322c
Kyrylo <dexter37814@gmail.com>
2024-10-25 10:36:10 +0300

    feat: change project model to car

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from './components/header/Header'
+import { Suspense } from 'react'
+import CarShow from './components/car-show/CarShow'
 
 const roboto = Roboto({
     weight: '400',
@@ -22,6 +24,7 @@ export default function RootLayout({
         <html lang='en' className={roboto.variable}>
             <body className='bg-background text-foreground'>
                 <Header />
+                <CarShow />
                 <main>{children}</main>
             </body>
         </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
 import bg from '../../public/background/home-background.png'
-import Model from './components/home-model/Model'
 
 export default function HomePage() {
     return (
         <section className='w-full h-screen relative'>
 
-            <Model/>
         </section>
     )
 }
