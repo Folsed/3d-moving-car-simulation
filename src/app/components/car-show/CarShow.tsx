e14f5867478d06c983d341baefb6f61ae3f1bf0e
Kyrylo <dexter37814@gmail.com>
2024-10-25 11:46:25 +0300

    feat: add textures and simple car component

--- src/app/components/car-show/CarShow.tsx
+++ src/app/components/car-show/CarShow.tsx
@@ -2,6 +2,7 @@
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
+import Ground from './Ground'
 
 const CarShow = () => {
     return (
@@ -19,7 +20,7 @@ const CarShow = () => {
 
                     <spotLight
                         color={[1, 0.25, 0.7]}
-                        intensity={1.5}
+                        intensity={150}
                         angle={0.6}
                         penumbra={0.5}
                         position={[5, 5, 0]}
@@ -29,18 +30,20 @@ const CarShow = () => {
 
                     <spotLight
                         color={[0.14, 0.5, 1]}
-                        intensity={2}
+                        intensity={200}
                         angle={0.6}
                         penumbra={0.5}
                         position={[-5, 5, 0]}
                         castShadow
                         shadow-bias={-0.0001}
                     />
+                    
+                    <Ground />
 
-                    <mesh>
+                    {/* <mesh>
                         <boxGeometry args={[1, 1, 1]} />
                         <meshBasicMaterial color={'red'} />
-                    </mesh>
+                    </mesh> */}
                 </Canvas>
             </Suspense>
         </section>
