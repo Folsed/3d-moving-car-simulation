114bb712c4a6a2584ff01ac8aa199e0abf416b49
Kyrylo <dexter37814@gmail.com>
2024-10-26 03:12:06 +0300

    bump: prod version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import bg from '../../public/background/home-background.png'
-
 export default function HomePage() {
     return <section className='relative h-screen w-full'></section>
 }
